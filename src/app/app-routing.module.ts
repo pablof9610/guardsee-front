1922b14fcf23e7449e10b4cb013c5856cc955e42
pafesan <pablof9610@gmail.com>
2025-05-17 23:39:10 -0300

    feat: add manage component and page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ProductComponent } from './pages/product/product.component';
+import { ManageComponent } from './pages/manage/manage.component';
 
 const routes: Routes = [
   {path: 'dashboard', component: DashboardComponent},
   {path: 'product', component: ProductComponent},
+  {path: 'manage', component: ManageComponent},
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
 ];
 
