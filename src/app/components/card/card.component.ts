9dfe1119dc0b4219a0402ff634efd8dbf0f6f802
pafesan <pablof9610@gmail.com>
2025-05-19 21:44:41 -0300

    refactor: change card component for showing general info

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -8,7 +8,8 @@ import { Component, Input } from "@angular/core";
 
 export class CardComponent {
   @Input() name: string = '';
-  @Input() quantity: string = '';
-  @Input() ticket: string = '';
-  @Input() icon: string = '';
+  @Input() vendor: string = '';
+  @Input() model: string = '';
+  @Input() serialNumber: string = '';
+  @Input() detail: string = '';
 }
