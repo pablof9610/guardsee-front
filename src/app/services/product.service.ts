1f664e9ceb7a32218e52c7e54bbea1a9b3a3e85b
pafesan <pablof9610@gmail.com>
2025-05-20 21:40:15 -0300

    feat: add getAllProductDistinct function to list DTO

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Product } from "../models/product";
+import { ProductListDTO } from "../models/ProductListDTO";
 
 @Injectable({
   providedIn: "root"
@@ -13,6 +14,10 @@ export class ProductService {
 
   getAllProduct(): Observable<Product[]> {
     return this.http.get<Product[]>(this.baseUrl, {responseType: 'json'})
+  }
+
+  getAllProductDistinct(): Observable<ProductListDTO[]> {
+    return this.http.get<ProductListDTO[]>(`${this.baseUrl}/distinct`, {responseType: 'json'})
   }
 
   saveProduct(product: Product): Observable<any> {
