92e3432f7b2b0bb14fe09547ef7af416a8cb8d27
pafesan <pablof9610@gmail.com>
2025-05-20 21:40:48 -0300

    feat: add audit page routing and import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ProductComponent } from './pages/product/product.component';
 import { ManageComponent } from './pages/manage/manage.component';
+import { AuditComponent } from './pages/audit/audit.component';
 
 const routes: Routes = [
   {path: 'dashboard', component: DashboardComponent},
   {path: 'product', component: ProductComponent},
   {path: 'manage', component: ManageComponent},
+  {path: 'audit', component: AuditComponent },
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
 ];
 
