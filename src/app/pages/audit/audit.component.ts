987828891476ee76bf0aeaeba8112763f684d316
pafesan <pablof9610@gmail.com>
2025-05-26 20:43:43 -0300

    feat: add delete product operation, data showing and style

--- src/app/pages/audit/audit.component.ts
+++ src/app/pages/audit/audit.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { Product } from "../../models/product";
 import { ProductService } from "../../services/product.service";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-audit',
@@ -22,6 +23,33 @@ export class AuditComponent {
             console.error('Error to find products ',err);
           }
         )
+    }
 
+    editProduct(serialNumber: string) {
+    }
+
+    deleteProduct(serialNumber: string) {
+      Swal.fire({
+        title: 'Tem certeza?',
+        text: 'Esta ação não poderá ser desfeita!',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: 'Sim',
+        cancelButtonText: 'Cancelar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          console.log(serialNumber)
+          this.productService.deleteProductBySerialNumber(serialNumber).subscribe(
+          (data) => {
+            console.log("Produto deletado, id retornado: ",data);
+            this.products = this.products.filter(e => e.serialNumber !== serialNumber);
+          },
+          (err) => {
+            console.log("Erro ao excluir produtos: ",err);
+        });
+        }
+      });
     }
 }
