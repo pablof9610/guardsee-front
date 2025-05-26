257e120799a4f8fa4153363beeabcb770a9af6eb
pafesan <pablof9610@gmail.com>
2025-05-26 20:46:24 -0300

    feat: add delete product operation

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Product } from "../models/product";
-import { ProductListDTO } from "../models/ProductListDTO";
+import { ProductStateDTO } from "../models/ProductStateDTO";
 
 @Injectable({
   providedIn: "root"
@@ -16,11 +16,15 @@ export class ProductService {
     return this.http.get<Product[]>(this.baseUrl, {responseType: 'json'})
   }
 
-  getAllProductDistinct(): Observable<ProductListDTO[]> {
-    return this.http.get<ProductListDTO[]>(`${this.baseUrl}/distinct`, {responseType: 'json'})
+  getAllProductDistinct(): Observable<ProductStateDTO[]> {
+    return this.http.get<ProductStateDTO[]>(`${this.baseUrl}/distinct`)
   }
 
   saveProduct(product: Product): Observable<any> {
     return this.http.post(this.baseUrl, product)
+  }
+
+  deleteProductBySerialNumber(serialNumber: string): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${serialNumber}`);
   }
 }
