2f305299679ce08744633e0b75e1fe533a650380
pafesan <pablof9610@gmail.com>
2025-05-28 22:04:53 -0300

    feat: add edit buttons and mechanism to show product states counted/style

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Product } from "../../models/product";
 
 @Component({
   selector: 'app-card',
@@ -12,4 +13,40 @@ export class CardComponent {
   @Input() model: string = '';
   @Input() serialNumber: string = '';
   @Input() detail: string = '';
+  @Input() ticket: string | undefined = '';
+  @Input() state: string | undefined = '';
+
+  @Output() edit = new EventEmitter<Product>();
+  @Output() delete = new EventEmitter<string>();
+
+  defineClass(){
+    switch (this.state?.toLowerCase()) {
+      case 'em uso':
+        return 'u';
+      case 'em estoque':
+        return 's';
+      case 'obsolescencia':
+        return 'o';
+      default:
+        return 'no-match';
+    }
+  }
+
+  onEdit() {
+    const product: Product = {
+      name: this.name,
+      vendor: this.vendor,
+      model: this.model,
+      serialNumber: this.serialNumber,
+      detail: this.detail,
+      ticket: this.ticket,
+      state: this.state
+    }
+
+    this.edit.emit(product);
+  }
+
+  onDelete() {
+    this.delete.emit(this.serialNumber);
+  }
 }
