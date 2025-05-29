a2cd336991f4893e1cfbd23c20df82d7a5268c96
pafesan <pablof9610@gmail.com>
2025-05-28 22:05:47 -0300

    refactor: change product DTO attributes showing strategy

--- src/app/components/card-dto/card-dto.component.ts
+++ src/app/components/card-dto/card-dto.component.ts
@@ -3,10 +3,13 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-card-dto',
   templateUrl: './card-dto.component.html',
-  styleUrl: './card-dto.component.scss'
+  styleUrls: ['./card-dto.component.scss']
 })
 
 export class CardDtoComponent {
   @Input() name: string = '';
-  @Input() quantity: string = '';
+  @Input() quantity: number = 0;
+  @Input() inStock: number = 0;
+  @Input() inUse: number = 0;
+  @Input() broken: number = 0;
 }
